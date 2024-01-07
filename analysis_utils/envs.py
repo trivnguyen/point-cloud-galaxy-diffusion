23879273a83609b2881f86756a5afe09ad019c7c
Tri Nguyen <tnguyen@ccalin011.flatironinstitute.org>
2024-01-07 15:27:14 -0500

    clean up

---
+++ analysis_utils/envs.py
@@ -0,0 +1,8 @@
+
+# import Path
+from pathlib import Path
+
+
+DEFAULT_RAW_DATASET_DIR = Path("/mnt/ceph/users/tnguyen/dark_camels/point-cloud-diffusion-datasets/raw_datasets/")
+DEFAULT_DATASET_DIR = Path("/mnt/ceph/users/tnguyen/dark_camels/point-cloud-diffusion-datasets/processed_datasets/")
+DEFAULT_FOF_CATALOG = Path("/mnt/ceph/users/jrose/MW_zooms/fixed_cosmo")
