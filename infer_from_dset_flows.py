fea4b6ce7f975f20db7bd052c32c2aa1ba6a8ecb
Tri Nguyen <tnguyen@ccalin011.flatironinstitute.org>
2024-04-11 16:14:29 -0400

    update inference script

--- infer_from_dset_flows.py
+++ infer_from_dset_flows.py
@@ -48,7 +48,19 @@ def infer(config: ConfigDict):
         config.data.n_particles,
         conditioning_parameters=config.data.conditioning_parameters
     )
+    _, _, flows_conditioning, flows_norm_dict = datasets.get_nbody_data(
+        config.data.dataset_root,
+        config.data.dataset_name,
+        config.data.n_features,
+        config.data.n_particles,
+        conditioning_parameters=config.data.flows_conditioning_parameters + config.data.flows_labels
+    )
     x = x * norm_dict['std'] + norm_dict['mean']
+
+    num_conditioning = len(config.data.conditioning_parameters)
+    num_flows_conditioning = len(config.data.flows_conditioning_parameters)
+    num_flows_labels = len(config.data.flows_labels)
+
 
     # load the VDM and the normalizing flows
     logging.info("Loading the VDM and the normalizing flows...")
@@ -56,12 +68,9 @@ def infer(config: ConfigDict):
     vdm, vdm_params = models.diffusion.VariationalDiffusionModel.from_path_to_model(
         path_to_model=path_to_vdm, norm_dict=norm_dict)
 
-    if config.flows_name is not None:
-        path_to_flows = Path(os.path.join(config.logdir, config.flows_name))
-        flows, flows_params = nsf.NeuralSplineFlow.from_path_to_model(
-            path_to_model=path_to_flows)
-    else:
-        flows, flows_params = None, None
+    path_to_flows = Path(os.path.join(config.logdir, config.flows_name))
+    flows, flows_params = nsf.NeuralSplineFlow.from_path_to_model(
+        path_to_model=path_to_flows)
 
     # Create the sampling function based on the normalizing flows
     @partial(jax.vmap, in_axes=(0, None, 0))
@@ -78,61 +87,77 @@ def infer(config: ConfigDict):
         return x_samples
 
     # Iterate over the entire dataset and start generation
-    truth_samples = []
-    truth_mask = []
-    gen_samples = []
-    gen_cond = []
-    gen_mask = []
-
     dset = datasets.make_dataloader(
-        x, conditioning, mask, batch_size=config.batch_size,
-        shuffle=False, repeat=False)
+        (x, conditioning, flows_conditioning, mask), batch_size=batch_size,
+        seed=42, shuffle=False, repeat=False)
     dset = create_input_iter(dset)
 
-    logging.info("Starting generation...")
+    truth_samples = []
+    truth_mask = []
+    vdm_samples = []
+    vdm_cond = []
+    vdm_mask = []
+    flows_samples = []
+    flows_cond = []
+
     for batch in tqdm(dset):
-        x_batch, cond_batch, mask_batch = batch[0], batch[1], batch[2]
-        x_batch = jnp.repeat(x_batch[0], config.n_repeats, axis=0)
-        cond_batch = jnp.repeat(cond_batch[0], config.n_repeats, axis=0)
-        truth_mask_batch = jnp.repeat(mask_batch[0], config.n_repeats, axis=0)
+        x_batch, cond_batch, flows_cond_batch, mask_batch = batch
+        x_batch = jnp.repeat(x_batch[0], num_repeats, axis=0)
+        cond_batch = jnp.repeat(cond_batch[0], num_repeats, axis=0)
+        flows_cond_batch = jnp.repeat(flows_cond_batch[0], num_repeats, axis=0)
+        truth_mask_batch = jnp.repeat(mask_batch[0], num_repeats, axis=0)
 
-        rng, _ = jax.random.split(rng)
+        # generate the flow samples
+        flows_samples_batch = sample_from_flow(
+            flows_cond_batch[:, :num_flows_conditioning], 1,
+            jax.random.split(rng, len(cond_batch))).squeeze()
 
-        # generate the number of particles using the flows
-        if flows is not None:
-            num_subhalos = 10**sample_from_flow(
-                cond_batch, 1, jax.random.split(rng, len(cond_batch))).squeeze()
-            num_subhalos = jnp.clip(num_subhalos, 1, config.data.n_particles)
-            num_subhalos = jnp.round(num_subhalos).astype(jnp.int32)
-            gen_mask_batch = create_mask(num_subhalos, config.data.n_particles)
-        else:
-            gen_mask_batch = truth_mask_batch
+        flows_labels_std = flows_norm_dict['cond_std'][num_flows_conditioning:]
+        flows_labels_mean = flows_norm_dict['cond_mean'][num_flows_conditioning:]
+        flows_samples_batch = flows_samples_batch * flows_labels_std + flows_labels_mean
 
-        # generate using the VDM
-        gen_samples.append(
+        # get the total number of particles
+        num_subhalos = 10**flows_samples_batch[..., log_num_subhalos_idx]
+        num_subhalos = jnp.clip(num_subhalos, 1, num_particles)
+        num_subhalos = jnp.round(num_subhalos).astype(jnp.int32)
+        vdm_mask_batch = create_mask(num_subhalos, num_particles)
+
+        # get the new conditioning vector
+        if len(flows_to_condition) > 0:
+            # make sure that the normalization is working correctly
+            cond_batch = cond_batch * norm_dict['cond_std'] + norm_dict['cond_mean']
+            cond_batch = cond_batch.at[:, condition_to_flows].set(
+                flows_samples_batch[:, flows_to_condition])
+            cond_batch = (cond_batch - norm_dict['cond_mean']) / norm_dict['cond_std']
+
+        vdm_samples.append(
             eval.generate_samples(
                 vdm=vdm,
                 params=vdm_params,
                 rng=rng,
                 n_samples=len(cond_batch),
-                n_particles=config.data.n_particles,
+                n_particles=num_particles,
                 conditioning=cond_batch,
-                mask=gen_mask_batch,
-                steps=config.steps,
+                mask=vdm_mask_batch,
+                steps=steps,
                 norm_dict=norm_dict,
-                boxsize=config.data.box_size,  # doesn't matter
+                boxsize=1,  # doesn't matter
             )
         )
-        gen_cond.append(cond_batch)
-        gen_mask.append(gen_mask_batch)
+        vdm_mask.append(vdm_mask_batch)
+        vdm_cond.append(cond_batch)
         truth_samples.append(x_batch)
         truth_mask.append(truth_mask_batch)
+        flows_cond.append(flows_cond_batch)
+        flows_samples.append(flows_samples_batch)
 
-    gen_samples = jnp.concatenate(gen_samples, axis=0)
-    gen_cond = jnp.concatenate(gen_cond, axis=0)
-    gen_mask = jnp.concatenate(gen_mask, axis=0)
+    vdm_samples = jnp.concatenate(vdm_samples, axis=0)
+    vdm_mask = jnp.concatenate(vdm_mask, axis=0)
+    vdm_cond = jnp.concatenate(vdm_cond, axis=0)
     truth_samples = jnp.concatenate(truth_samples, axis=0)
     truth_mask = jnp.concatenate(truth_mask, axis=0)
+    flows_samples = jnp.concatenate(flows_samples, axis=0)
+    flows_cond = jnp.concatenate(flows_cond, axis=0)
 
     # Save the samples
     if config.output_name is None:
@@ -148,7 +173,8 @@ def infer(config: ConfigDict):
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
     logging.info("Saving the generated samples to %s", output_path)
     np.savez(
-        output_path, samples=gen_samples, cond=gen_cond, mask=gen_mask,
+        output_path, samples=vdm_samples, cond=vdm_cond, mask=vdm_mask,
+        flows_samples=flows_samples, flows_cond=flows_cond,
         truth=truth_samples, truth_mask=truth_mask
     )
 
